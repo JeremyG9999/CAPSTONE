f1e95e6f345efc8d9e8884a942ac8d89ba9f9f5b
jeremyg999 <jgaglio1@kent.edu>
2025-04-05 19:30:17 -0400

    UI Sprint 1
    - I made the form a box.
    - I slightly contrasted the background.
    - I made the buttons green.
    - I did this for update user page, login page, and create account page.

--- models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class User(db.Model): # just login 
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    
